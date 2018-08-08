d5d9503829fc2ef77c3981e34e3fa2b5b66c04d4
jonathan senior <jonosenior@gmail.com>
2018-08-08 11:39:48 +0200

    Finish basic restaurant functional tests, without #create

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -21,13 +21,30 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should destroy" do
+  test "should get new" do
+    get new_restaurant_path
+    assert_response :success
+  end
+
+  test "should destroy and redirect" do
     assert_difference 'Restaurant.count', -1 do
       delete restaurant_path(@dajiale.id)
     end
     assert_redirected_to root_url
-
   end
+
+  # test "should create restaurant" do
+  #   # get new_restaurant_path
+  #   assert_difference('Restaurant.count') do
+  #     # post new_restaurant_path(id: 152349817)
+  #     post restaurant_path, name: "Da Jia Le",
+  #                      address: "new@new.new",
+  #                      cuisine: "first",
+  #                      description: "last",
+  #                      id: 2
+  #   end
+  #   assert_redirected_to restaurant_path(@dajiale)
+  # end
 
 
 end
